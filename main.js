4a6459f7c8f2908e4a23ab734feef99d7b010c31
shemalucien <shemalucien5@gmail.com>
2021-08-22 11:01:39 +0200

    first upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const msg = 'Whoops! It looks like you forgot to add your email';
+const form = document.querySelector('form');
+const button = form.querySelector('[type="submit"]');
+
+const error = document.createElement('p');
+error.classList.add('error_text');
+error.textContent = 'Please provide a valid email address';
+
+function checkEmail(e) {
+	e.preventDefault();
+	const email = e.target.querySelector('[type="email"]').value;
+	if (!validateEmail(email)) {
+		form.classList.add('error');
+		form.insertBefore(error, button);
+	} else {
+		form.removeChild(error);
+		form.classList.remove('error');
+	}
+}
+function validateEmail(email) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+form.addEventListener('submit', checkEmail);
